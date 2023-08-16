5ddc58476da9927ffd92d4c0b8385abf6aa7ebe7
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2023-08-16 15:33:13 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-function preload() {
 
+mustache_x=0;
+mustache_y=0;
+
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
-function setup() {
-    canvas = createCanvas(500, 500);
+function setup(){
+    canvas = createCanvas(500,500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
 }
 
-function draw() {
-
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache,mustache_x,mustache_y,60,45);
 }
 
-function saveImage() {
-    save ('filtered_image.png');
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        mustache_x = results[0].pose.nose.x-30;
+        console.log("nose y: "+results[0].pose.nose.y);
+        mustache_y = results[0].pose.nose.y;
+    };
 }
